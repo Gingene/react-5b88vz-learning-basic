e411a4002feac9487807ab97cd5a203b680a04aa
Gingene <90446775+Gingene@users.noreply.github.com>
2023-03-14 10:30:45 +0800

    Ternary Operators

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React, { useState } from 'react';
 import './style.css';
 
-// https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png
-
-const pokemons = [
-  {
-    dexId: 1,
-    name: 'Bulbasaur',
-  },
-  {
-    dexId: 2,
-    name: 'Ivysaur',
-  },
-  {
-    dexId: 3,
-    name: 'Venusaur',
-  },
-];
-
 export default function App() {
-  const [value, setValue] = useState('');
+  const [showText, setShowText] = useState(false);
+
+
   return (
     <div className="App">
-      {pokemons.map((pokemon) => (
-        <>
-          <p>{pokemon.name}</p>
-          <img
-            src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${pokemon.dexId}.png`}
-          />
-        </>
-      ))}
+      <button onClick={() => setShowText(!showText)}>Toggle</button>
+      {/* {showText ? <p>Hello World</p> : null} */}
+      {/* {showText ? <SayHello /> : null} */}
+      {showText && <SayHello />}
     </div>
   );
 }
+
+const SayHello = () => <h1>Hello</h1>;
