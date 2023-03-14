fcdccbcb260fb7b88ee9f2f195664ba4c3ff0b9a
Gingene <90446775+Gingene@users.noreply.github.com>
2023-03-14 10:19:48 +0800

    Template String

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-// memo useCallback useMemo
+// https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png
+
+const pokemons = [
+  {
+    dexId: 1,
+    name: 'Bulbasaur',
+  },
+  {
+    dexId: 2,
+    name: 'Ivysaur',
+  },
+  {
+    dexId: 3,
+    name: 'Venusaur',
+  },
+];
 
 export default function App() {
-  // const [value, setValue] = useState('');
-  // const inputRef = useRef(null);
-  const cyanRef = useRef(null);
-  const orangeRef = useRef(null);
-
-  console.log('App render...');
-
-  const submitHandler = () => {
-    console.log(inputRef.current.value);
-    inputRef.current.type = 'password';
-  };
-
-  const goOrange = () => {
-    window.scrollTo({
-      top: orangeRef.current.offsetTop,
-      behavior: 'smooth',
-    });
-  };
-  const goCyan = () => {
-    window.scrollTo({
-      top: cyanRef.current.offsetTop,
-      behavior: 'smooth',
-    });
-  };
-
+  const [value, setValue] = useState('');
   return (
     <div className="App">
-      {/* <div className="ref-teaching">
-        <input type="text" ref={inputRef} />
-        <button onClick={submitHandler}>Sumbit</button>
-        <br />
-      </div> */}
-
-      {/* offsetTop */}
-      <div style={{ height: '600px', backgroundColor: 'cyan' }} ref={cyanRef}>
-        <button onClick={goOrange}>Go Orange</button>
-      </div>
-      <div
-        style={{ height: '600px', backgroundColor: 'orange' }}
-        ref={orangeRef}
-      >
-        <button onClick={goCyan}>Go Cyan</button>
-      </div>
-      <div style={{ height: '600px', backgroundColor: 'red' }} ref={orangeRef}>
-        <button onClick={goCyan}>Go Cyan</button>
-      </div>
-      {/* 每次使用者在輸入input的時候 你就要去檢查他輸入的文字(value) 有沒有符合自己定義的規則(即時檢查)就不要使用useRef 反之輸入完再檢查就使用useRef*/}
+      {pokemons.map((pokemon) => (
+        <>
+          <p>{pokemon.name}</p>
+          <img
+            src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${pokemon.dexId}.png`}
+          />
+        </>
+      ))}
     </div>
   );
 }
