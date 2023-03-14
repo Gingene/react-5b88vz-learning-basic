6397b19532705a65873738dccd5dd44b7d1247ba
Gingene <90446775+Gingene@users.noreply.github.com>
2023-03-14 09:07:52 +0800

    使用useRef操作DOM元素

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useRef } from 'react';
 import './style.css';
 
 // memo useCallback useMemo
 
 export default function App() {
-  const [value, setValue] = useState('');
+  // const [value, setValue] = useState('');
+  // const inputRef = useRef(null);
+  const cyanRef = useRef(null);
+  const orangeRef = useRef(null);
 
-  console.log('App Render...');
+  console.log('App render...');
 
-  // const expensiveValue = useMemo(() => {}, []);
+  const submitHandler = () => {
+    console.log(inputRef.current.value);
+    inputRef.current.type = 'password';
+  };
 
-  const ManyItem = useMemo(() => {
-    console.log('ManyItem Render...');
-    const array = new Array(100).fill(1);
-    return (
-      <>
-        {array.map((_, i) => (
-          <p>{i}</p>
-        ))}
-      </>
-    );
-  }, []);
+  const goOrange = () => {
+    window.scrollTo({
+      top: orangeRef.current.offsetTop,
+      behavior: 'smooth',
+    });
+  };
+  const goCyan = () => {
+    window.scrollTo({
+      top: cyanRef.current.offsetTop,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <div className="App">
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+      {/* <div className="ref-teaching">
+        <input type="text" ref={inputRef} />
+        <button onClick={submitHandler}>Sumbit</button>
+        <br />
+      </div> */}
 
-      <p>{value}</p>
-
-      {/* <ManyItem /> */}
-      {ManyItem}
+      {/* offsetTop */}
+      <div style={{ height: '600px', backgroundColor: 'cyan' }} ref={cyanRef}>
+        <button onClick={goOrange}>Go Orange</button>
+      </div>
+      <div
+        style={{ height: '600px', backgroundColor: 'orange' }}
+        ref={orangeRef}
+      >
+        <button onClick={goCyan}>Go Cyan</button>
+      </div>
+      <div style={{ height: '600px', backgroundColor: 'red' }} ref={orangeRef}>
+        <button onClick={goCyan}>Go Cyan</button>
+      </div>
+      {/* 每次使用者在輸入input的時候 你就要去檢查他輸入的文字(value) 有沒有符合自己定義的規則(即時檢查)就不要使用useRef 反之輸入完再檢查就使用useRef*/}
     </div>
   );
 }
